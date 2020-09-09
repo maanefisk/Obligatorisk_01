986e14713267492f5d9042ecaa071ecc921fa392
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-09-09 10:56:07 +0200

    Trying to make it work

--- Slektstre/Slektstre/Class1.cs
+++ Slektstre/Slektstre/Class1.cs
@@ -10,16 +10,20 @@ namespace Slektstre
         private int _Id;
         private string _FirstName;
         private string _SurName;
-        private string _BirthYear;
-        private string _YearOfDeath;
+        private int _BirthYear;
+        private int _YearOfDeath;
+        private string _Father;
+        private string _Mother;
 
-        public Person (int Id, string FirstName, string SurName, string BirthYear, string YearOfDeath)
+        public Person (int Id, string FirstName, string SurName, int BirthYear, int YearOfDeath, string Father, string Mother)
         {
             _Id = Id;
             _FirstName = FirstName;
             _SurName = SurName;
             _BirthYear = BirthYear;
             _YearOfDeath = YearOfDeath;
+            _Father = Father;
+            _Mother = Mother;
         }
     }
 }
