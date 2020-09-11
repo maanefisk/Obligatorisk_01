1e4a577e39a108bcca5cc88cf44d9d21857b67ae
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-09-11 11:42:37 +0200

    FamilyApp

--- Slektstre/Slektstre/Class1.cs
+++ Slektstre/Slektstre/Class1.cs
@@ -7,23 +7,13 @@ namespace Slektstre
 {
     class Person
     {
-        private int _Id;
-        private string _FirstName;
-        private string _SurName;
-        private int _BirthYear;
-        private int _YearOfDeath;
-        private string _Father;
-        private string _Mother;
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public int BirthYear;
+        public int YearOfDeath;
+        public Person Father;
+        public Person Mother;
 
-        public Person (int Id, string FirstName, string SurName, int BirthYear, int YearOfDeath, string Father, string Mother)
-        {
-            _Id = Id;
-            _FirstName = FirstName;
-            _SurName = SurName;
-            _BirthYear = BirthYear;
-            _YearOfDeath = YearOfDeath;
-            _Father = Father;
-            _Mother = Mother;
-        }
     }
 }
--- Slektstre/Slektstre/Program.cs
+++ Slektstre/Slektstre/Program.cs
@@ -6,14 +6,18 @@ namespace Slektstre
     {
         static void Main(string[] args)
         {
-            var sverreMagnus = new Person(1, "Sverre Magnus", null, 2005, null);
-            var ingridAlexandra = new Person(2,"Ingrid Alexandra",null, 2004);
-            var haakon = new Person(3, "Haakon Magnus",null,1973);
+            string args1 = Console.ReadLine();
+            if (args1 == "hjelp") Console.WriteLine("Hjelp");
+            if (args1 == "list") Console.WriteLine("Listemetode");
+            if (args1 == "vis <id>") Console.WriteLine("Spesifik Person");
+            var sverreMagnus = new Person { Id = 1, FirstName = "Sverre Magnus", BirthYear = 2005 };
+            var ingridAlexandra = new Person { Id = 2, FirstName = "Ingrid Alexandra", BirthYear = 2004 };
+            var haakon = new Person { Id = 3, FirstName = "Haakon Magnus", BirthYear = 1973 };
             var metteMarit = new Person { Id = 4, FirstName = "Mette-Marit", BirthYear = 1973 };
             var marius = new Person { Id = 5, FirstName = "Marius", LastName = "Borg Høiby", BirthYear = 1997 };
             var harald = new Person { Id = 6, FirstName = "Harald", BirthYear = 1937 };
             var sonja = new Person { Id = 7, FirstName = "Sonja", BirthYear = 1937 };
-            var olav = new Person { Id = 8, FirstName = "Olav", BirthYear = 1903 };
+            var olav = new Person { Id = 8, FirstName = "Olav", BirthYear = 1903 , YearOfDeath = 1991 };
 
             sverreMagnus.Father = haakon;
             sverreMagnus.Mother = metteMarit;
