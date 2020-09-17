ca95c329b5b6c90b6916df746c75e599760cca67
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-09-17 20:39:03 +0200

    Remove the tests

--- Slektstre/Slektstre_Test/Persontest.cs
+++ Slektstre/Slektstre_Test/Persontest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Slektstre;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Slektstre_Test
@@ -20,23 +20,6 @@ namespace Slektstre_Test
             Assert.AreEqual(expectedDescription, actualDescription);
         }
         public void TestAllFields()
-        {
-            var p = new Person
-            {
-                Id = 17,
-                FirstName = "Ola",
-                LastName = "Nordmann",
-                BirthYear = 2000,
-                DeathYear = 3000,
-                Father = new Person() { Id = 23, FirstName = "Per" },
-                Mother = new Person() { Id = 29, FirstName = "Lise" },
-            };
-
-            var actualDescription = p.GetDescription();
-            var expectedDescription = "Ola Nordmann (Id=17) Født: 2000 Død: 3000 Far: Per (Id=23) Mor: Lise (Id=29)";
-
-            Assert.AreEqual(expectedDescription, actualDescription);
-        }
 
     }
 }
