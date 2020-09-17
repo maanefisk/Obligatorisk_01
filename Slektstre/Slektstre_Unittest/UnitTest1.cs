ff905182fcfb0e5eeb18d8bf8095559eb8de36f4
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-09-17 20:45:56 +0200

    Remove again, wrong version!

---
+++ Slektstre/Slektstre_Unittest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Slektstre;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Slektstre_Unittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
